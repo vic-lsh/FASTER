bc23025754baf5135f551d28b8bda873c9592b68
Yuhong Zhong <yuhong.zhong@columbia.edu>
2022-10-19 07:13:00 -0700

    shuffle_free_list.cc: Support non-THP

--- cc/benchmark-dir/shuffle_free_list.cc
+++ cc/benchmark-dir/shuffle_free_list.cc
@@ -18,6 +18,9 @@
 #include <fcntl.h>
 #include <linux/page_coloring.h>
 
+#define PAGE_SHIFT 12UL
+#define PAGE_SIZE (1UL << PAGE_SHIFT)
+
 #define HUGE_PAGE_SHIFT 21UL
 #define HUGE_PAGE_SIZE (1UL << HUGE_PAGE_SHIFT)
 #define BITMAP_SIZE ((NR_COLORS + sizeof(uint64_t) * 8 - 1) / (sizeof(uint64_t) * 8))
@@ -59,7 +62,7 @@ void convert_list_to_bitmap(char *list, uint64_t *bitmap) {
 
 uint64_t addr_translate(int fd, void *ptr) {
   uint64_t virtual_addr = (uint64_t) ptr;
-  uint64_t virtual_pfn = virtual_addr / 4096;
+  uint64_t virtual_pfn = virtual_addr / PAGE_SIZE;
   size_t offset = virtual_pfn * sizeof(uint64_t);
 
   /*
@@ -73,15 +76,15 @@ uint64_t addr_translate(int fd, void *ptr) {
   BUG_ON((page & (1UL << 62UL)) != 0);  // shoud not be swapped
   BUG_ON((page & 0x7fffffffffffffUL) == 0);
 
-  uint64_t physical_addr = ((page & 0x7fffffffffffffUL) * 4096)
-                           + (virtual_addr % 4096);
-  BUG_ON((physical_addr % 4096) != (virtual_addr % 4096));
+  uint64_t physical_addr = ((page & 0x7fffffffffffffUL) * PAGE_SIZE)
+                           + (virtual_addr % PAGE_SIZE);
+  BUG_ON((physical_addr % PAGE_SIZE) != (virtual_addr % PAGE_SIZE));
   return physical_addr;
 }
 
-uint64_t get_page_color(uint64_t phys_addr) {
+uint64_t get_page_color(uint64_t phys_addr, uint64_t page_size) {
   uint64_t dram_addr = phys_addr % DRAM_SIZE_PER_NODE;
-  uint64_t dram_index = dram_addr >> HUGE_PAGE_SHIFT;
+  uint64_t dram_index = dram_addr / page_size;
   uint64_t tmp_1 = dram_index % NR_COLORS;
   uint64_t tmp_2 = dram_index / NR_COLORS;
   return (tmp_1 + tmp_2) % NR_COLORS;
@@ -94,33 +97,41 @@ bool color_is_set(uint64_t *colormask, uint64_t color) {
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2 && argc != 5) {
-    printf("Usage: %s <size of memory (GB)> <nid> <color list (e.g., 0-7,12)> <max occupancy>\n", argv[0]);
+  if (argc != 2 && argc != 3 && argc != 6) {
+    printf("Usage: %s <size of memory (GB)> <thp> <nid> <color list (e.g., 0-7,12)> <max occupancy>\n", argv[0]);
     exit(1);
   }
 
   uint64_t size_GB = atol(argv[1]);
   uint64_t size = size_GB << 30UL;
-  uint64_t num_pages = size / HUGE_PAGE_SIZE;
+
+  bool thp = true;  // assume THP by default
+  if (argc > 2)
+    thp = atoi(argv[2]) != 0;
+
+  uint64_t page_size = thp ? HUGE_PAGE_SIZE : PAGE_SIZE;
+  uint64_t num_pages = size / page_size;
 
   uint64_t nid;
   uint64_t colormask[BITMAP_SIZE];
   uint64_t max_occupancy;
-  if (argc == 5) {
-    nid = atol(argv[2]);
+  if (argc == 6) {
+    nid = atol(argv[3]);
     memset(colormask, 0, sizeof(colormask));
-    convert_list_to_bitmap(argv[3], colormask);
-    max_occupancy = atol(argv[4]);
+    convert_list_to_bitmap(argv[4], colormask);
+    max_occupancy = atol(argv[5]);
     BUG_ON(max_occupancy == 0);
   } else {
     BUG_ON(argc != 2);
   }
 
-  printf("Allocating %ld 2MB pages...\n", num_pages);
-  uint8_t *buffer = (uint8_t *) aligned_alloc(HUGE_PAGE_SIZE, size);
+  printf("Allocating %ld pages...\n", num_pages);
+  uint8_t *buffer = (uint8_t *) aligned_alloc(page_size, size);
   BUG_ON(buffer == NULL);
-  int ret = madvise(buffer, size, MADV_HUGEPAGE);
-  BUG_ON(ret != 0);
+  if (thp) {
+    int ret = madvise(buffer, size, MADV_HUGEPAGE);
+    BUG_ON(ret != 0);
+  }
 
   printf("Triggering random page faults...\n");
   uint64_t *index_arr = (uint64_t *) malloc(sizeof(*index_arr) * num_pages);
@@ -131,28 +142,38 @@ int main(int argc, char *argv[]) {
   shuffle(index_arr, index_arr + num_pages, default_random_engine(rd()));
   for (uint64_t i = 0; i < num_pages; ++i) {
     uint64_t index = index_arr[i];
-    memset(buffer + index * HUGE_PAGE_SIZE, 0, HUGE_PAGE_SIZE);
+    memset(buffer + index * page_size, 0, page_size);
   }
 
-  if (argc == 2)
+  if (argc <= 3)
     return 0;
 
   unordered_map<uint64_t, uint64_t> map;
   int fd = open("/proc/self/pagemap", O_RDONLY);
   BUG_ON(fd < 0);
   for (uint64_t i = 0; i < num_pages; ++i) {
-    uint64_t phys_addr = addr_translate(fd, buffer + i * HUGE_PAGE_SIZE);
-    BUG_ON(phys_addr / (DRAM_SIZE_PER_NODE * NR_PMEM_CHUNK) != nid);
+    uint64_t phys_addr = addr_translate(fd, buffer + i * page_size);
+    uint64_t actual_nid = phys_addr / (DRAM_SIZE_PER_NODE * NR_PMEM_CHUNK);
+    if (actual_nid != nid) {
+      printf("NUMA ID mismatch for phys_addr 0x%lx: expected %ld, got %ld\n", phys_addr, nid, actual_nid);
+    }
+    // BUG_ON(phys_addr / (DRAM_SIZE_PER_NODE * NR_PMEM_CHUNK) != nid);
 
-    uint64_t color = get_page_color(phys_addr);
-    BUG_ON(!color_is_set(colormask, color));
+    uint64_t color = get_page_color(phys_addr, page_size);
+    if (!color_is_set(colormask, color)) {
+      printf("Color mismatch for phys_addr 0x%lx: got %ld\n", phys_addr, color);
+    }
+    // BUG_ON(!color_is_set(colormask, color));
 
     uint64_t dram_addr = phys_addr % DRAM_SIZE_PER_NODE;
     if (map.find(dram_addr) == map.end()) {
       map[dram_addr] = 0;
     }
     map[dram_addr]++;
-    BUG_ON(map[dram_addr] > max_occupancy);
+    if (map[dram_addr] > max_occupancy) {
+      printf("Max occupancy exceeded for phys_addr 0x%lx: expected %ld, got %ld\n", max_occupancy, map[dram_addr]);
+    }
+    // BUG_ON(map[dram_addr] > max_occupancy);
   }
   BUG_ON(close(fd) != 0);
   return 0;
