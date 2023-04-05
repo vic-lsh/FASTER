dea5765383d7c98fc79b14764a2b9c69f8d12a0a
vic-shihang-li <40274755+vic-shihang-li@users.noreply.github.com>
2023-04-05 13:09:06 -0400

    add client-side multiplexing

--- cs/samples/FasterLogQuerier/Program.cs
+++ cs/samples/FasterLogQuerier/Program.cs
@@ -7,9 +7,22 @@ using System.Collections.Generic;
 using K4os.Compression.LZ4;
 using System.Linq;
 using FasterLogData;
+using System.Collections.Concurrent;
 
 namespace FasterLogQuerier
 {
+    class Request
+    {
+        public byte[] Payload { get; }
+        public int FlowId { get; }
+
+        public Request(byte[] payload, int id)
+        {
+            Payload = payload;
+            FlowId = id;
+        }
+    }
+
     public class Program
     {
 
@@ -19,9 +32,12 @@ namespace FasterLogQuerier
 
         static void Main()
         {
-            // var c = new QueryClient(SERVER, PORT);
+            var conn = new QueryServerConn(SERVER, PORT, numFlows: NUM_QUERIERS + 1);
+            new Thread(() => conn.Serve()).Start();
 
-            var firstClient = new QueryClient(SERVER, PORT, usePerfSources: false);
+            var id = 0;
+
+            var firstClient = new QueryClient(id++, conn, usePerfSources: false);
             Thread.Sleep(150_000);
             Console.WriteLine("low rate client begins query");
             new Thread(() =>
@@ -35,6 +51,7 @@ namespace FasterLogQuerier
             }).Start();
 
             Thread.Sleep(115_000);
+            // Thread.Sleep(5_000);
             Console.WriteLine("perf clients begin query");
 
             var thrs = new List<Thread>();
@@ -42,11 +59,10 @@ namespace FasterLogQuerier
             {
                 var t = new Thread(() =>
                 {
-                    var c = new QueryClient(SERVER, PORT, usePerfSources: true);
+                    var c = new QueryClient(id++, conn, usePerfSources: true);
                     var sw = new Stopwatch();
                     sw.Start();
-                    // while (sw.ElapsedMilliseconds < 70_000)
-                    while (sw.ElapsedMilliseconds < 115_000)
+                    while (sw.ElapsedMilliseconds < 70_000)
                     {
                         c.DoQuery();
                     }
@@ -60,113 +76,181 @@ namespace FasterLogQuerier
                 t.Join();
             }
         }
-
     }
 
-    class QueryClient : IDisposable
+    class QueryServerConn : IDisposable
     {
-        // static readonly ulong LOOKBACK_NS = 10_000_000_000;
-        static readonly ulong LOOKBACK_NS = 10_000_000_000;
+        public HashSet<ulong> Sources;
+        public HashSet<ulong> PerfSources;
 
-        Random rand = new Random();
-        HashSet<ulong> sources;
-        byte[] buf;
-        TcpClient client;
-        NetworkStream stream;
+        TcpClient Client;
+        NetworkStream Stream;
+        byte[] Buf;
 
-        public QueryClient(string server, int port)
+        ConcurrentQueue<Request> reqs = new ConcurrentQueue<Request>();
+        ConcurrentQueue<byte[]>[] responses = new ConcurrentQueue<byte[]>[11];
+
+        public QueryServerConn(string server, int port, int numFlows)
         {
-            try
+            responses = new ConcurrentQueue<byte[]>[numFlows];
+            for (var i = 0; i < responses.Length; i++)
             {
-                client = ConnectWithRetry(server, port);
-                Console.WriteLine("Client connected");
-                stream = client.GetStream();
-
-                buf = new byte[1L << 21];
-
-                var input = new byte[4];
-                for (var i = 0; i < input.Length; i++)
-                {
-                    input[i] = 0;
-                }
-
-                var itr = 0;
-                while (true)
-                {
-                    var start = Stopwatch.GetTimestamp();
-                    // Write(stream, input);
-                    stream.Write(input, 0, 4);
-
-                    ReadMessage(stream, buf);
-                    // ReadNBytes(stream, buf, 165000);
-                    var end = Stopwatch.GetTimestamp();
-                    if (itr % 20 == 0)
-                    {
-                        Console.WriteLine("{0}", end - start);
-                    }
-                    itr++;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("QueryClientException: {0}", e);
+                responses[i] = new ConcurrentQueue<byte[]>();
             }
 
-        }
-
-        public QueryClient(string server, int port, bool usePerfSources)
-        {
             try
             {
-                client = ConnectWithRetry(server, port);
-                client.NoDelay = true;
+                Client = ConnectWithRetry(server, port);
+                Client.NoDelay = true;
                 Console.WriteLine("Client connected");
-                stream = client.GetStream();
-
-                buf = new byte[1L << 21];
+                Stream = Client.GetStream();
+                Buf = new byte[1L << 21];
 
                 // ExperimentStart message
-                var msgSize = ReadMessage(stream, buf);
-                var expStartMsg = ExperimentStart.Decode(buf);
-                if (usePerfSources)
-                {
-                    sources = expStartMsg.PerfSources;
-                    Console.WriteLine($"Choosing from {expStartMsg.PerfSources.Count} perf sources");
-                }
-                else
-                {
-                    sources = expStartMsg.Sources;
-                    Console.WriteLine($"Choosing from {expStartMsg.Sources.Count} sources");
-                }
+                var msgSize = ReadMessage(Stream, Buf);
+                var expStartMsg = ExperimentStart.Decode(Buf);
+                PerfSources = expStartMsg.PerfSources;
+                Sources = expStartMsg.Sources;
+                Console.WriteLine($"Choosing from {Sources.Count} sources and {PerfSources.Count} perf sources");
             }
             catch (Exception e)
             {
-                Console.WriteLine("QueryClientException: {0}", e);
+                Console.WriteLine("Exception while starting a query server conn: {0}", e);
+            }
+        }
+
+        public void Serve()
+        {
+            while (true)
+            {
+                if (reqs.TryDequeue(out var request))
+                {
+                    Write(Stream, request.Payload);
+
+                    var size = ReadMessage(Stream, Buf);
+
+                    var response = new byte[size];
+                    Buffer.BlockCopy(Buf, 0, response, 0, (int)size);
+                    responses[request.FlowId].Enqueue(response);
+                }
+            }
+        }
+
+        public void Write(Request req)
+        {
+            reqs.Enqueue(req);
+        }
+
+        public byte[] Read(int flowID)
+        {
+            while (true)
+            {
+                if (responses[flowID].TryDequeue(out var result))
+                {
+                    return result;
+                }
             }
         }
 
         public void Dispose()
         {
-            client.Dispose();
+            Client.Dispose();
+        }
+
+        static TcpClient ConnectWithRetry(string server, int port, int retry = 10)
+        {
+            while (retry > 0)
+            {
+                try
+                {
+                    TcpClient client = new TcpClient(server, port);
+                    return client;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(1000);
+                    retry--;
+                }
+            }
+
+            throw new Exception("Failed to connect to server");
+        }
+
+        static void Write(NetworkStream stream, byte[] bytes)
+        {
+            var sizeBytes = new byte[4];
+            BinaryPrimitives.WriteUInt32BigEndian(sizeBytes.AsSpan(), (uint)bytes.Length);
+
+            stream.Write(sizeBytes, 0, 4);
+            stream.Write(bytes, 0, bytes.Length);
+        }
+
+        static uint ReadMessage(NetworkStream stream, byte[] bytes)
+        {
+            ReadNBytes(stream, bytes, 4);
+            var size = BinaryPrimitives.ReadUInt32BigEndian(new Span<byte>(bytes, 0, 4));
+            ReadNBytes(stream, bytes, (int)size);
+            return size;
+        }
+
+        static void ReadNBytes(NetworkStream stream, byte[] bytes, int size)
+        {
+            if (bytes.Length < size)
+            {
+                throw new Exception($"Buffer is too small: buffer size {bytes.Length} requested size {size}");
+            }
+
+            var start = 0;
+            var remaining = size;
+            while (remaining > 0)
+            {
+                var nread = stream.Read(bytes, start, remaining);
+                start += nread;
+                remaining -= nread;
+                if (remaining > 0 && nread == 0)
+                {
+                    throw new Exception($"Stream terminated before reading the full message: requested {size}, {remaining} not received");
+                }
+            }
+        }
+    }
+
+    class QueryClient
+    {
+        static readonly ulong LOOKBACK_NS = 10_000_000_000;
+
+        Random Rand = new Random();
+        QueryServerConn Conn;
+        int Id;
+        HashSet<ulong> Sources;
+
+        public QueryClient(int id, QueryServerConn conn, bool usePerfSources)
+        {
+            Id = id;
+            Conn = conn;
+
+            try
+            {
+                if (usePerfSources)
+                {
+                    Sources = conn.PerfSources;
+                    Console.WriteLine($"Choosing from {conn.PerfSources.Count} perf sources");
+                }
+                else
+                {
+                    Sources = conn.Sources;
+                    Console.WriteLine($"Choosing from {conn.Sources.Count} sources");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("QueryClientException: {0}", e);
+            }
         }
 
         // Issue and process a query. Returning whether the client should continue
         // issue another query.
         public bool DoQuery()
-        {
-            return DoQueryInternal();
-            //             try
-            //             {
-            //                 return DoQueryInternal();
-            //             }
-            //             catch (Exception e)
-            //             {
-            //                 Console.WriteLine($"Query exception: {e}");
-            //                 return true;
-            //             }
-        }
-
-        bool DoQueryInternal()
         {
             var sampleCounts = 0;
             var blocksScanned = 0;
@@ -174,14 +258,15 @@ namespace FasterLogQuerier
 
             Console.WriteLine($"Issuing query on source {query.SourceId} {query.MinTimestamp} {query.MaxTimestamp}");
 
+            var startNs = Stopwatch.GetTimestamp();
             Stopwatch sw = new Stopwatch();
             sw.Start();
             while (true)
             {
-                Write(stream, query.Encode());
+                Write(query.Encode());
 
                 var start = Stopwatch.GetTimestamp();
-                var msgSize = ReadMessage(stream, buf);
+                var buf = Read();
                 if (!BlockReply.IsServerActive(buf))
                 {
                     return false;
@@ -192,7 +277,7 @@ namespace FasterLogQuerier
                 // }
 
                 // the first byte is the active byte
-                var block = new Span<byte>(buf, 1, (int)msgSize - 1);
+                var block = new Span<byte>(buf, 1, (int)buf.Length - 1);
 
                 start = Stopwatch.GetTimestamp();
                 var done = ProcessBlock(block, query, out ulong nextAddr, out int sourceSampleCount);
@@ -209,13 +294,9 @@ namespace FasterLogQuerier
 
                 // ask server for this block
                 query.NextBlockAddr = nextAddr;
-                // if (blocksScanned % 10 == 0)
-                // {
-                //     Console.WriteLine($"processing block {blocksScanned}, next addr {nextAddr}");
-                // }
             }
 
-            Console.WriteLine($"Query done, {sw.ElapsedMilliseconds} ms {sampleCounts} samples {blocksScanned} blocks scanned");
+            Console.WriteLine($"start {startNs} dur {sw.ElapsedMilliseconds} ms {sampleCounts} samples {blocksScanned} blocks scanned");
 
             return true;
         }
@@ -276,7 +357,6 @@ namespace FasterLogQuerier
             }
         }
 
-
         Query MakeNewQuery()
         {
             var maxTs = (ulong)Stopwatch.GetTimestamp();
@@ -286,64 +366,17 @@ namespace FasterLogQuerier
 
         ulong ChooseNewSource()
         {
-            return sources.ElementAt(rand.Next(sources.Count));
+            return Sources.ElementAt(Rand.Next(Sources.Count));
         }
 
-        static void Write(NetworkStream stream, byte[] bytes)
+        void Write(byte[] bytes)
         {
-            var sizeBytes = new byte[4];
-            BinaryPrimitives.WriteUInt32BigEndian(sizeBytes.AsSpan(), (uint)bytes.Length);
-
-            stream.Write(sizeBytes, 0, 4);
-            stream.Write(bytes, 0, bytes.Length);
+            Conn.Write(new Request(bytes, Id));
         }
 
-        static uint ReadMessage(NetworkStream stream, byte[] bytes)
+        byte[] Read()
         {
-            ReadNBytes(stream, bytes, 4);
-            var size = BinaryPrimitives.ReadUInt32BigEndian(new Span<byte>(bytes, 0, 4));
-            ReadNBytes(stream, bytes, (int)size);
-            return size;
-        }
-
-        static void ReadNBytes(NetworkStream stream, byte[] bytes, int size)
-        {
-            if (bytes.Length < size)
-            {
-                throw new Exception($"Buffer is too small: buffer size {bytes.Length} requested size {size}");
-            }
-
-            var start = 0;
-            var remaining = size;
-            while (remaining > 0)
-            {
-                var nread = stream.Read(bytes, start, remaining);
-                start += nread;
-                remaining -= nread;
-                if (remaining > 0 && nread == 0)
-                {
-                    throw new Exception($"Stream terminated before reading the full message: requested {size}, {remaining} not received");
-                }
-            }
-        }
-
-        static TcpClient ConnectWithRetry(string server, int port, int retry = 10)
-        {
-            while (retry > 0)
-            {
-                try
-                {
-                    TcpClient client = new TcpClient(server, port);
-                    return client;
-                }
-                catch (Exception)
-                {
-                    Thread.Sleep(1000);
-                    retry--;
-                }
-            }
-
-            throw new Exception("Failed to connect to server");
+            return Conn.Read(Id);
         }
     }
 }
